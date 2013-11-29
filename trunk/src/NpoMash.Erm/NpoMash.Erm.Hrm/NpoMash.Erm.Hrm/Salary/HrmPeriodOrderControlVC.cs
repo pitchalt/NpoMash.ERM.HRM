0f667e726a94c47194b9f8d3ced48291c0e3953a
Student001 <Student001@294e8926-5120-4385-9bc8-594b46af50e8>
2013-11-29 14:53:52 +0000

    Ковтушенко проверяем почему я Student001 =)

--- trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/Salary/HrmPeriodOrderControlVC.cs
+++ trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/Salary/HrmPeriodOrderControlVC.cs
@@ -36,7 +36,7 @@ namespace NpoMash.Erm.Hrm.Salary
 
         private void FillHrmPeriodOrderCOntrol_Execute(object sender, SimpleActionExecuteEventArgs e)
         {
-            //комментарий Ковтушенко проверяем почему я Student001 =)
+            //комментарий
         }
     }
 }
