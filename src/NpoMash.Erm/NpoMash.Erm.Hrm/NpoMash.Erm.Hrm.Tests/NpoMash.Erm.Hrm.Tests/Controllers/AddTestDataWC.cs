a89e40c7e18e353d48d126411207d63e8dbf3814
DeminD002 <DeminD002@294e8926-5120-4385-9bc8-594b46af50e8>
2013-11-30 16:50:13 +0000

    Добавлен дамп БД в качетсве иточника для тоестовых наборов.

--- src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/Controllers/AddTestDataWC.cs
+++ src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/Controllers/AddTestDataWC.cs
@@ -1,6 +1,9 @@
 using System;
+using System.IO;
 using System.Linq;
 using System.Text;
+using IntecoAG.Erm.HRM;
+using IntecoAG.Erm.FM.Order;
 using DevExpress.ExpressApp;
 using DevExpress.Data.Filtering;
 using System.Collections.Generic;
@@ -8,6 +11,7 @@ using DevExpress.Persistent.Base;
 using DevExpress.ExpressApp.Utils;
 using DevExpress.ExpressApp.Layout;
 using DevExpress.ExpressApp.Actions;
+using IntecoAG.Erm.HRM.Organization;
 using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.Templates;
 using DevExpress.Persistent.Validation;
@@ -27,7 +31,26 @@ namespace NpoMash.Erm.Hrm.Tests.Controllers
 
         private void addNewData_Execute(object sender, SimpleActionExecuteEventArgs e)
         {
+            #region Creating objectSpace and Collections
 
+            IObjectSpace objectSpace = Application.CreateObjectSpace();
+            fmCOrder fmCorder = objectSpace.CreateObject<fmCOrder>();
+            HrmSalaryPayType hrmSalaryPayType = objectSpace.CreateObject<HrmSalaryPayType>();
+            Department department = objectSpace.CreateObject<Department>();
+
+            var fmCorderCollection = objectSpace.GetObjects<fmCOrder>();
+            var hrmSalaryPayTypeConnection = objectSpace.GetObjects<HrmSalaryPayType>();
+            var departmentCollection = objectSpace.GetObjects<Department>();
+            
+            #endregion
+
+            string line;
+            StreamReader streamReader = new StreamReader("Dep.dat");
+            while (!streamReader.EndOfStream)
+            {
+                line = streamReader.ReadLine();
+            }
+            streamReader.Close();
         }
     }
 }
