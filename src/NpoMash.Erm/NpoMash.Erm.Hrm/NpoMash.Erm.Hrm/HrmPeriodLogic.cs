a502c46d2d1ab9b7271978bbaf7d5b54a854da80
Student001 <Student001@294e8926-5120-4385-9bc8-594b46af50e8>
2013-12-08 10:31:53 +0000

    - Функция поиска последнего добавленного периода

--- src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/HrmPeriodLogic.cs
+++ src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/HrmPeriodLogic.cs
@@ -17,9 +17,37 @@ namespace NpoMash.Erm.Hrm
     [Persistent("HrmPeriodLogic")]
     public static class HrmPeriodLogic : BaseObject {
 
-    public static void createPeriod(IObjectSpace os, HrmPeriod obj) {  }
 
-    public void addMonth() {
+        public static HrmPeriod findLastPeriod(IObjectSpace os) {
+
+            var period_list = os.GetObjects<HrmPeriod>();
+            HrmPeriod last_period=null;
+            var maxYear = period_list.Max(Period => Period.Year);
+            List<HrmPeriod> HrmPeriodMaxYearsCollection = new List<HrmPeriod>(); //Список периодов с максимальным годом
+            //Формируем этот лист
+            foreach (var a in period_list) {
+                if (a.Year == maxYear) {
+                    HrmPeriodMaxYearsCollection.Add(a);
+                }
+            }
+            var maxMonth = HrmPeriodMaxYearsCollection.Max(myProd => myProd.Month); //Максимальный месяц в этой коллекции
+            //var last_period = os.CreateObject<HrmPeriod>();
+
+            foreach (var a in period_list) {
+                if ((a.Year == maxYear) && (a.Month == maxMonth)) {
+                    last_period=a;
+                }
+            }
+            return last_period; // Возваращем последний период
+        }
+
+    public static void createPeriod(IObjectSpace os, HrmPeriod obj) { 
+    
+    
+    
+    }
+
+    /*public void addMonth() {
         Int16 m = Month;
         m++;
         if (m > 12) {
@@ -30,7 +58,7 @@ namespace NpoMash.Erm.Hrm
         }
         SetPropertyValue<Int16>("Month", ref _Month, m);
     }
-        
+        */
 
 
         public HrmPeriodLogic(Session session): base(session){ }
