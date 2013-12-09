d57d2cb2ac17f4479bb3d94ff61be9a9903e66f4
DeminD002 <DeminD002@294e8926-5120-4385-9bc8-594b46af50e8>
2013-12-09 11:51:06 +0000

    Подправил логику тестового контроллера

--- src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/Controllers/TestWCLogic.cs
+++ src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/Controllers/TestWCLogic.cs
@@ -15,19 +15,18 @@ namespace NpoMash.Erm.Hrm.Tests.Controllers {
 
     public static class TestWCLogic {
 
+        private const int _HRM_PERIOD_COUNT = 3;
+
         public static int intRandomValue() {
             var random = new Random();
             return random.Next( 1000, 10000000 );
         }
 
         public static void addTestData( IObjectSpace a_object_space ) {
-            int period_count = 3;
-            Int16 start_month = 0;
-            Int16 start_year = 2000;
             var period_list = new List<HrmPeriod>();
-            for ( int i = 0 ; i < period_count ; i++ ) {
+            for ( int i = 0 ; i < _HRM_PERIOD_COUNT ; i++ ) {
                 var period = HrmPeriodLogic.createPeriod( a_object_space );
-                if ( i < period_count - 1 ) {
+                if ( i < _HRM_PERIOD_COUNT - 1 ) {
                     period.Status = HrmPeriodStatus.closed;
                 }
                 else {
