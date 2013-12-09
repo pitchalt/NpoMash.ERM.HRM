e4fa5add59bdd4f3a78f7e5eb960591331b1dfa8
DeminD002 <DeminD002@294e8926-5120-4385-9bc8-594b46af50e8>
2013-12-09 11:03:49 +0000

    Теперь можно добавлять тестовые наборы с учетом новой логики для HrmPeriod

---
+++ src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/Controllers/TesterWC.Designer.cs
@@ -0,0 +1,36 @@
+namespace NpoMash.Erm.Hrm.Tests.Controllers {
+    partial class TesterWC {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose( bool disposing ) {
+            if ( disposing && ( components != null ) ) { components.Dispose(); }
+            base.Dispose( disposing );
+        }
+
+        #region Component Designer generated code
+
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.PopulateAction = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            // 
+            // PopulateAction
+            // 
+            this.PopulateAction.Caption = "TesterWC_PopulateAction";
+            this.PopulateAction.Category = "Tools";
+            this.PopulateAction.ConfirmationMessage = null;
+            this.PopulateAction.Id = "TesterWC_PopulateAction";
+            this.PopulateAction.ImageName = null;
+            this.PopulateAction.Shortcut = null;
+            this.PopulateAction.Tag = null;
+            this.PopulateAction.TargetObjectsCriteria = null;
+            this.PopulateAction.TargetViewId = null;
+            this.PopulateAction.ToolTip = null;
+            this.PopulateAction.TypeOfView = null;
+            this.PopulateAction.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.PopulateDB_Execute);
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.Actions.SimpleAction PopulateAction;
+    }
+}
