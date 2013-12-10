a330debb9a7945dd8742f2ac15545723982aecc1
DeminD002 <DeminD002@294e8926-5120-4385-9bc8-594b46af50e8>
2013-12-10 11:11:49 +0000

    Исправлена кодировка тестового скрипта, изменено заполнение базы с учетом новой логики

--- src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/Controllers/TestWCLogic.cs
+++ src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/Controllers/TestWCLogic.cs
@@ -14,22 +14,38 @@ namespace NpoMash.Erm.Hrm.Tests.Controllers {
 
     public static class TestWCLogic {
 
-        private const int _HRM_PERIOD_COUNT = 3;
+        private const int _REFERENCE_COUNT = 10;
+        private const int _ALLOCPARAMETER_COUNT = 3;
 
         public static int intRandomValue() {
-            var random = new Random();
-            return random.Next( 1000, 10000000 );
+            return new Random().Next( 1000, 1000000 );
+        }
+
+        public static void referenceClassesGenerate( IObjectSpace local_object_space ) {
+            for ( int i = 0 ; i < _REFERENCE_COUNT ; i++ ) {
+                var fmCorder = local_object_space.CreateObject<fmCOrder>();
+                var hrmSalaryPayType = local_object_space.CreateObject<HrmSalaryPayType>();
+                int type_control = new Random().Next(1,4);
+                int type_constancy = new Random().Next( 1, 3 );
+                fmCorder.Code = Convert.ToString( intRandomValue() );
+                if ( type_control == 1 ) { fmCorder.TypeControl = fmCOrderTypeCOntrol.FOT; }
+                if ( type_control == 2 ) { fmCorder.TypeControl = fmCOrderTypeCOntrol.No_Ordered; }
+                if ( type_control == 3 ) { fmCorder.TypeControl = fmCOrderTypeCOntrol.TrudEmk_FOT; }
+                if ( type_constancy == 1 ) { fmCorder.TypeConstancy = fmCOrdertypeConstancy.Null; }
+                if ( type_constancy == 2 ) { fmCorder.TypeConstancy = fmCOrdertypeConstancy.One; }
+                fmCorder.NormKB = Convert.ToDecimal( intRandomValue() );
+                fmCorder.NormOZM = Convert.ToDecimal( intRandomValue() );
+                hrmSalaryPayType.Code = Convert.ToString( intRandomValue() );
+                hrmSalaryPayType.Name = Convert.ToString( intRandomValue() );
+            }
         }
 
         public static void addTestData( IObjectSpace a_object_space ) {
-            var period_list = new List<HrmPeriod>();
-            for ( int i = 0 ; i < _HRM_PERIOD_COUNT ; i++ ) {
-                var period = HrmPeriodLogic.createPeriod( a_object_space );
-                if ( i < _HRM_PERIOD_COUNT - 1 ) {
-                    period.Status = HrmPeriodStatus.closed;
-                }
-                else {
-                    period.Status = HrmPeriodStatus.Opened;
+            referenceClassesGenerate( a_object_space );
+            for ( int i = 0 ; i < _ALLOCPARAMETER_COUNT ; i++ ) {
+                var alloc_parameter = AllocParametersLogic.createParameters( a_object_space );
+                foreach ( var each in a_object_space.GetObjects<HrmPeriod>( null, true ) ) {
+                    each.Status = HrmPeriodStatus.closed;
                 }
             }
         }
