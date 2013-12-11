ab1e8b88cc99bd981f9f61f436e6b15d2b0cf5cc
pitchalt <pitchalt@294e8926-5120-4385-9bc8-594b46af50e8>
2013-12-11 19:04:03 +0000

    Для Демина Контроллер и Экшен тестируются из  NUnit  обращаю внимание, что это тестирование WindowController про  ViewController написано в примере http://documentation.devexpress.com/#Xaf/CustomDocument3218

--- trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/StructuralTests/AllocParameterVCUnitTest.cs
+++ trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/StructuralTests/AllocParameterVCUnitTest.cs
@@ -14,28 +14,48 @@ using NpoMash.Erm.Hrm.Tests.Controllers;
 namespace NpoMash.Erm.Hrm.Tests.StructuralTests {
 
 
-    class TestApplication : XafApplication {
-        protected override LayoutManager CreateLayoutManagerCore( bool simple ) {
-            return null;
-        }
-    }
-
     [TestFixture]
     public class AllocParameterVCUnitTest {
-        IObjectSpace object_space;
+//        IObjectSpace object_space;
         TestApplication application;
         [SetUp]
         public void setUp() {
-            IObjectSpaceProvider object_space_provider = new XPObjectSpaceProvider( new MemoryDataStoreProvider() );
+            IObjectSpaceProvider object_space_provider = new XPObjectSpaceProvider(new MemoryDataStoreProvider());
             application = new TestApplication();
             ModuleBase test_module = new ModuleBase();
             test_module.AdditionalExportedTypes.Add(typeof(HrmPeriod));
-            application.Modules.Add( test_module );
+            application.Modules.Add(test_module);
             application.Setup("TestApplication", object_space_provider);
-            object_space = application.CreateObjectSpace();
+//            object_space = application.CreateObjectSpace();
         }
 
         [Test]
-        public void TestHrmPeriodAllocParameter() { }
+        public void TestHrmPeriodAllocParameter() {
+            TesterWC test_wc = application.CreateController<TesterWC>();
+            ICollection<Controller> controllers = new List<Controller>();
+            controllers.Add(test_wc);
+            Window test_window = application.CreateWindow(TemplateContext.ApplicationWindow, controllers, true);
+            test_wc.PopulateAction.DoExecute();
+            IObjectSpace os = application.CreateObjectSpace();
+            IList<HrmPeriod> periods = new List<HrmPeriod>(os.GetObjects<HrmPeriod>().OrderBy(x => x.Year * 100 + x.Month));
+            Assert.AreEqual(periods.Count, 3);
+            Assert.AreEqual(periods[0].Year, 2013);
+            Assert.AreEqual(periods[1].Year, 2014);
+            Assert.AreEqual(periods[2].Year, 2014);
+            Assert.AreEqual(periods[2].Month, 2);
+        }
+    }
+
+    class TestApplication : XafApplication {
+        public TestApplication() : base() {
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.TestsWindowsFormsApplication_DatabaseVersionMismatch);
+        }
+        protected override LayoutManager CreateLayoutManagerCore(bool simple) {
+            return null;
+        }
+        private void TestsWindowsFormsApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
     }
 }
