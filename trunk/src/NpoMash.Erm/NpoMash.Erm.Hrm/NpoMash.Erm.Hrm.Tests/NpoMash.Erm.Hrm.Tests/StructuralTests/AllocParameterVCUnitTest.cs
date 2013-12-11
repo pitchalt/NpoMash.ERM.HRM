acb80aa315fdb60d95d13478593d1522e5e4da8b
DeminD002 <DeminD002@294e8926-5120-4385-9bc8-594b46af50e8>
2013-12-11 16:14:41 +0000

    Добавил Unit Test

---
+++ trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/StructuralTests/AllocParameterVCUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Xpo;
+using DevExpress.ExpressApp.Layout;
+
+using NpoMash.Erm.Hrm.Salary;
+using NpoMash.Erm.Hrm.Tests.Controllers;
+
+namespace NpoMash.Erm.Hrm.Tests.StructuralTests {
+
+
+    class TestApplication : XafApplication {
+        protected override LayoutManager CreateLayoutManagerCore( bool simple ) {
+            return null;
+        }
+    }
+
+    [TestFixture]
+    public class AllocParameterVCUnitTest {
+        IObjectSpace object_space;
+        TestApplication application;
+        [SetUp]
+        public void setUp() {
+            IObjectSpaceProvider object_space_provider = new XPObjectSpaceProvider( new MemoryDataStoreProvider() );
+            application = new TestApplication();
+            ModuleBase test_module = new ModuleBase();
+            test_module.AdditionalExportedTypes.Add(typeof(HrmPeriod));
+            application.Modules.Add( test_module );
+            application.Setup("TestApplication", object_space_provider);
+            object_space = application.CreateObjectSpace();
+        }
+
+        [Test]
+        public void TestHrmPeriodAllocParameter() { }
+    }
+}
