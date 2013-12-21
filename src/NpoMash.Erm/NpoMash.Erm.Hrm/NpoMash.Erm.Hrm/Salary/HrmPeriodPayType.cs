4015379e5c5a7134af85d0189074f9fbe12fe82a
KovtD003 <KovtD003@294e8926-5120-4385-9bc8-594b46af50e8>
2013-12-21 20:48:14 +0000

    больше нельзя добавлять пустые коды оплат

--- src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/Salary/HrmPeriodPayType.cs
+++ src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/Salary/HrmPeriodPayType.cs
@@ -29,6 +29,7 @@ namespace NpoMash.Erm.Hrm.Salary {
 
         private HrmSalaryPayType _PayType;  //Ñâÿçü ñ HrmSalaryPayType
         [Indexed("AllocParameter", Unique = true)]
+        [RuleRequiredField(DefaultContexts.Save)]
         public HrmSalaryPayType PayType {
             get { return _PayType; }
             set { SetPropertyValue<HrmSalaryPayType>("PayType", ref _PayType, value); }
