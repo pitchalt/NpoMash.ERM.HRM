a1b7c42eea65266a43829d9b0be6f0a4949de5a6
Student001 <Student001@294e8926-5120-4385-9bc8-594b46af50e8>
2013-12-22 12:54:08 +0000

    - экшн

--- trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/Salary/HrmMatrixVC.cs
+++ trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/Salary/HrmMatrixVC.cs
@@ -14,34 +14,58 @@ using DevExpress.ExpressApp.Templates;
 using DevExpress.Persistent.Validation;
 using DevExpress.ExpressApp.SystemModule;
 using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.Editors;
+using System.Collections;
+
 
 namespace NpoMash.Erm.Hrm.Salary {
     public partial class HrmMatrixVC : ViewController {
 
+        public enum DATA_SOURCE {
+            generateTestData=0,
+            dataFromFile=1,
+            dataFromServer=2
+        }
 
 
+        private ChoiceActionItem setDataSourceItem;
+        private ChoiceActionItem setFileTypeItem;
+        
+        public HrmMatrixVC() { 
+            InitializeComponent(); 
+            RegisterActions(components); 
+           
+            GetSourceDataAction.Items.Clear(); 
+            setDataSourceItem=new ChoiceActionItem(CaptionHelper.GetMemberCaption(typeof(HrmPeriod), "Source"), null);
+            GetSourceDataAction.Items.Add(setDataSourceItem);
+            FillItemWithEnumValues(setDataSourceItem, typeof(DATA_SOURCE));
 
+            setFileTypeItem = new ChoiceActionItem(CaptionHelper.GetMemberCaption(typeof(HrmPeriod), "FileType"), null);
+            GetSourceDataAction.Items.Add(setFileTypeItem);
+            FillItemWithEnumValues(setDataSourceItem, typeof(DATA_SOURCE));
+        
+        }
 
+        private void FillItemWithEnumValues(ChoiceActionItem setDataSourceItem, Type enumType)  {
+      
+            foreach(object current in Enum.GetValues(enumType)) {
+         EnumDescriptor ed = new EnumDescriptor(enumType);
+         ChoiceActionItem item = new ChoiceActionItem(ed.GetCaption(current), current);
+        
+      }
+   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        public HrmMatrixVC() { InitializeComponent(); RegisterActions(components); }
+        
         protected override void OnActivated() { base.OnActivated(); }
         protected override void OnViewControlsCreated() { base.OnViewControlsCreated(); }
         protected override void OnDeactivated() { base.OnDeactivated(); }
 
         private void ImportSourceData_Execute(object sender, ParametrizedActionExecuteEventArgs e) {
+
+        }
+
+        private void GetSourceDataAction_Execute(object sender, SingleChoiceActionExecuteEventArgs e) {
+
 
         }
     }
