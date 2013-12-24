3fe981fb0adbdfc6248555f5e51400c8a63d3413
KovtD003 <KovtD003@294e8926-5120-4385-9bc8-594b46af50e8>
2013-12-24 17:24:12 +0000

    генерация плановой матрицы

--- trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/Salary/HrmMatrixLogic.cs
+++ trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/Salary/HrmMatrixLogic.cs
@@ -23,42 +23,75 @@ namespace NpoMash.Erm.Hrm.Salary {
 
 
         static public HrmMatrixAllocPlan setTestData(IObjectSpace os,HrmPeriod current_period ) {
-            var random = new Random();
-            var order_list = os.GetObjects<fmCOrder>();
-            var order_count = order_list.Count();
+            Random random = new Random();
+            //IList<fmCOrder> order_list = os.GetObjects<fmCOrder>();
+            const DEPARTMENT_GROUP_DEP GROUP_DEP_OF_MATRIX = DEPARTMENT_GROUP_DEP.KB;
+            List<HrmMatrixColumn> columns = new List<HrmMatrixColumn>();
+            List<HrmMatrixRow> rows = new List<HrmMatrixRow>();
+            HrmMatrixAllocPlan plan_matrix = os.CreateObject<HrmMatrixAllocPlan>();
 
-            //Ячейки
+            for (int i = 0; i < 20; i++) {
+                Department new_department = os.CreateObject<Department>();
+                //var new_column = os.CreateObject<HrmMatrixColumn>();
+                new_department.Code = Convert.ToString(random.Next(10000, 99999));
+                int type_group = random.Next(1, 2);
+                if (type_group == 1) { new_department.GroupDep = DEPARTMENT_GROUP_DEP.KB; }
+                if (type_group == 2) { new_department.GroupDep = DEPARTMENT_GROUP_DEP.OZM; }
+                //new_column.Sum = random.Next(10000, 99999);
+                //new_column.Department = new_department;
+                //columns.Add(new_column);
+            }
+            foreach (fmCOrder current_order in os.GetObjects<fmCOrder>()){
+                HrmMatrixRow current_row = os.CreateObject<HrmMatrixRow>();
+                current_row.Sum = 0;
+                current_row.Matrix = plan_matrix;
+                plan_matrix.Rows.Add(current_row);
+                current_row.Order = current_order;
+                HrmMatrixColumn current_column = null;
+                foreach (Department current_department in os.GetObjects<Department>()) {
+                    if (current_department.GroupDep == GROUP_DEP_OF_MATRIX) {
+                        foreach (HrmMatrixColumn col in plan_matrix.Columns)
+                            if (col.Department == current_department) current_column = col;
+                        if (current_column == null) {
+                            current_column = os.CreateObject<HrmMatrixColumn>();
+                            current_column.Department = current_department;
+                            current_column.Matrix = plan_matrix;
+                            current_column.Sum = 0;
+                        }
+                        HrmMatrixCell new_cell = os.CreateObject<HrmMatrixCell>();
+                        new_cell.Time = Convert.ToInt16(random.Next(100, 500));
+                        new_cell.Sum = Convert.ToInt16(random.Next(100, 500));
+                        new_cell.Column = current_column;
+                        new_cell.Row = current_row;
+                        current_row.Cells.Add(new_cell);
+                        current_column.Cells.Add(new_cell);
+                        current_row.Sum += new_cell.Time;
+                        current_column.Sum += new_cell.Time;
+                    }
+                }
+            }
+            /*//Ячейки
             var cells = new List<HrmMatrixCell>();
             for (int i = 0; i <= order_count * 10; i++) {
                 var new_cell = os.CreateObject<HrmMatrixCell>();
                 new_cell.Sum = random.Next(10000, 99999);
                 new_cell.Time = Convert.ToInt16(random.Next(100, 999)); 
                 cells.Add(new_cell);
-            }
+            }*/
 
         //Подразделений и колонок матрицы
-            var columns = new List<HrmMatrixColumn>();
-            for (int i = 0; i < 10; i++) {
-                var new_department = os.CreateObject<Department>();
-                var new_column = os.CreateObject<HrmMatrixColumn>();
-                new_department.Code = Convert.ToString(random.Next(10000, 99999));
-                int type_group = random.Next(1, 2);
-                if (type_group == 1) { new_department.GroupDep = DEPARTMENT_GROUP_DEP.KB; }
-                if (type_group == 2) { new_department.GroupDep = DEPARTMENT_GROUP_DEP.OZM; }
-                new_column.Sum=random.Next(10000, 99999);
-                new_column.Department = new_department;
-                columns.Add(new_column);
-            }
+            
+            
 
             //Теперь хочу строк 
-            var rows = new List<HrmMatrixRow>();
+            /*
             foreach (var order in order_list) {
                 var new_row = os.CreateObject<HrmMatrixRow>();
                 new_row.Sum = random.Next(10000, 99999);
                 new_row.Order = order;
                 rows.Add(new_row);
-            }
-            
+            }*/
+            /*
             int a = 0;
             foreach (var c in columns) {
                 c.Cells.Add(cells[a]);
@@ -68,34 +101,26 @@ namespace NpoMash.Erm.Hrm.Salary {
             foreach (var r in rows) {
                 r.Cells.Add(cells[a]);
                 a++;
-            }
+            }*/
         //Создаем плановую матрицу
-            var plan_matrix=os.CreateObject<HrmMatrixAllocPlan>();
             plan_matrix.Type = HRM_MATRIX_TYPE.Matrix;
             plan_matrix.TypeMatrix = HRM_MATRIX_TYPE_MATRIX.Planned;
             plan_matrix.GroupDep = HRM_MATRIX_GROUP_DEP.KB;
             plan_matrix.Status = HRM_MATRIX_STATUS.Opened;
-            plan_matrix.IterationNumber = HRM_MATRIX_ITERATION_NUMBER.Five;
+            plan_matrix.IterationNumber = HRM_MATRIX_ITERATION_NUMBER.One;
             plan_matrix.Variant = HRM_MATRIX_VARIANT.ProportionsMethod;
-
-            foreach (var c in columns) {
+            plan_matrix.Period = current_period;
+            current_period.Matrixs.Add(plan_matrix);
+            
+            /*foreach (var c in columns) {
                 plan_matrix.Columns.Add(c);
             }
 
             foreach (var r in rows) {
                 plan_matrix.Rows.Add(r);
-            }
-
-            current_period.Matrixs.Add(plan_matrix);
-
+            }*/
             return plan_matrix;
         }
-   
-        
-        
-        
-
-
 
         public HrmMatrixLogic(Session session): base(session) { }
         public override void AfterConstruction() {base.AfterConstruction();}
