136a55a6fcd2693008421647f8b4582fd9d99806
DeminD002 <DeminD002@294e8926-5120-4385-9bc8-594b46af50e8>
2013-12-24 17:20:56 +0000

    Генерация для функционала Ковтушенко

--- src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/Controllers/TestWCLogic.cs
+++ src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/Controllers/TestWCLogic.cs
@@ -15,10 +15,28 @@ namespace NpoMash.Erm.Hrm.Tests.Controllers {
     public static class TestWCLogic {
 
         private const int _REFERENCE_COUNT = 10;
+        private const int _DEPARTMENT_COUNT = 20;
         private const int _ALLOCPARAMETER_COUNT = 3;
+
+        public static void addTimeSheets( IObjectSpace local_object_space ) {
+            foreach ( var each in local_object_space.GetObjects<HrmPeriod>() ) {
+                var time_sheet = local_object_space.CreateObject<HrmTimeSheet>();
+                var time_sheet_deps = local_object_space.CreateObject<HrmTimeSheetDep>();
+            }
+        }
 
         public static void referenceClassesGenerate( IObjectSpace local_object_space ) {
             var random = new Random();
+            for ( int i = 0 ; i < _DEPARTMENT_COUNT ; i++ ) {
+                var department = local_object_space.CreateObject<Department>();
+                department.Code = Convert.ToString( random.Next( 1000, 4001 ) );
+                if ( Convert.ToDecimal( i ) < System.Math.Round( Convert.ToDecimal( _DEPARTMENT_COUNT / 2 ) ) ) {
+                    department.GroupDep = DEPARTMENT_GROUP_DEP.KB;
+                }
+                else {
+                    department.GroupDep = DEPARTMENT_GROUP_DEP.OZM;
+                }
+            }
             for ( int i = 0 ; i < _REFERENCE_COUNT ; i++ ) {
                 var fmCorder = local_object_space.CreateObject<fmCOrder>();
                 var hrmSalaryPayType = local_object_space.CreateObject<HrmSalaryPayType>();
@@ -33,7 +51,7 @@ namespace NpoMash.Erm.Hrm.Tests.Controllers {
                 fmCorder.NormKB = Convert.ToDecimal( random.Next( 1000, 100000 ) );
                 fmCorder.NormOZM = Convert.ToDecimal( random.Next( 1000, 100000 ) );
                 hrmSalaryPayType.Code = Convert.ToString( random.Next( 1000, 100000 ) );
-                hrmSalaryPayType.Name = Convert.ToString( random.Next( 1000, 100000 ) );
+                hrmSalaryPayType.Name = Convert.ToString( random.Next( 1000, 100000 ) );               
             }
         }
 
