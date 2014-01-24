b3e270f8e9381dd1cbf49f9a924b198c5b4b1e49
pitchalt <pitchalt@294e8926-5120-4385-9bc8-594b46af50e8>
2014-01-24 20:23:34 +0000

    В HrmSalaryTaskMatrixReductionVC теперь активируются только нужные элементы в списках доступных методов в экшенах приведения и утверждения, также переделал все на базе enum.

--- trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/Salary/HrmSalaryTaskMatrixReductionVC.Designer.cs
+++ trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/Salary/HrmSalaryTaskMatrixReductionVC.Designer.cs
@@ -24,81 +24,45 @@
         /// </summary>
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
-            DevExpress.ExpressApp.Actions.ChoiceActionItem choiceActionItem1 = new DevExpress.ExpressApp.Actions.ChoiceActionItem();
-            DevExpress.ExpressApp.Actions.ChoiceActionItem choiceActionItem2 = new DevExpress.ExpressApp.Actions.ChoiceActionItem();
-            DevExpress.ExpressApp.Actions.ChoiceActionItem choiceActionItem3 = new DevExpress.ExpressApp.Actions.ChoiceActionItem();
-            DevExpress.ExpressApp.Actions.ChoiceActionItem choiceActionItem4 = new DevExpress.ExpressApp.Actions.ChoiceActionItem();
-            DevExpress.ExpressApp.Actions.ChoiceActionItem choiceActionItem5 = new DevExpress.ExpressApp.Actions.ChoiceActionItem();
-            DevExpress.ExpressApp.Actions.ChoiceActionItem choiceActionItem6 = new DevExpress.ExpressApp.Actions.ChoiceActionItem();
-            this.BringingMatrixInReduc = new DevExpress.ExpressApp.Actions.SingleChoiceAction(this.components);
-            this.AcceptCoercedMatrix = new DevExpress.ExpressApp.Actions.SingleChoiceAction(this.components);
+            this.BringingMatrixInReducAction = new DevExpress.ExpressApp.Actions.SingleChoiceAction(this.components);
+            this.AcceptCoercedMatrixAction = new DevExpress.ExpressApp.Actions.SingleChoiceAction(this.components);
             this.ExportCoercedMatrix = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
             // 
-            // BringingMatrixInReduc
+            // BringingMatrixInReducAction
             // 
-            this.BringingMatrixInReduc.Caption = "Bringing TYPE_MATIX In Reduc";
-            this.BringingMatrixInReduc.ConfirmationMessage = null;
-            this.BringingMatrixInReduc.Id = "BringingMatrixInReduc";
-            this.BringingMatrixInReduc.ImageName = null;
-            choiceActionItem1.Caption = "1)Метод пропорции";
-            choiceActionItem1.ImageName = null;
-            choiceActionItem1.Shortcut = null;
-            choiceActionItem1.ToolTip = null;
-            choiceActionItem2.Caption = "2)Метод минимизаци числа отклонений";
-            choiceActionItem2.ImageName = null;
-            choiceActionItem2.Shortcut = null;
-            choiceActionItem2.ToolTip = null;
-            choiceActionItem3.Caption = "3)Метод минимизации максимальных отклонений";
-            choiceActionItem3.ImageName = null;
-            choiceActionItem3.Shortcut = null;
-            choiceActionItem3.ToolTip = null;
-            this.BringingMatrixInReduc.Items.Add(choiceActionItem1);
-            this.BringingMatrixInReduc.Items.Add(choiceActionItem2);
-            this.BringingMatrixInReduc.Items.Add(choiceActionItem3);
-            this.BringingMatrixInReduc.ItemType = DevExpress.ExpressApp.Actions.SingleChoiceActionItemType.ItemIsOperation;
-            this.BringingMatrixInReduc.Shortcut = null;
-            this.BringingMatrixInReduc.ShowItemsOnClick = true;
-            this.BringingMatrixInReduc.Tag = null;
-            this.BringingMatrixInReduc.TargetObjectsCriteria = null;
-            this.BringingMatrixInReduc.TargetObjectType = typeof(NpoMash.Erm.Hrm.Salary.HrmSalaryTaskMatrixReduction);
-            this.BringingMatrixInReduc.TargetViewId = null;
-            this.BringingMatrixInReduc.TargetViewType = DevExpress.ExpressApp.ViewType.DetailView;
-            this.BringingMatrixInReduc.ToolTip = null;
-            this.BringingMatrixInReduc.TypeOfView = typeof(DevExpress.ExpressApp.DetailView);
-            this.BringingMatrixInReduc.Execute += new DevExpress.ExpressApp.Actions.SingleChoiceActionExecuteEventHandler(this.BringingMatrixInReduc_Execute);
+            this.BringingMatrixInReducAction.Caption = "Bringing TYPE_MATIX In Reduc";
+            this.BringingMatrixInReducAction.ConfirmationMessage = null;
+            this.BringingMatrixInReducAction.Id = "HrmSalaryTYaskMatrixReductionVC_BringingMatrixInReducAction";
+            this.BringingMatrixInReducAction.ImageName = null;
+            this.BringingMatrixInReducAction.ItemType = DevExpress.ExpressApp.Actions.SingleChoiceActionItemType.ItemIsOperation;
+            this.BringingMatrixInReducAction.Shortcut = null;
+            this.BringingMatrixInReducAction.ShowItemsOnClick = true;
+            this.BringingMatrixInReducAction.Tag = null;
+            this.BringingMatrixInReducAction.TargetObjectsCriteria = null;
+            this.BringingMatrixInReducAction.TargetObjectType = typeof(NpoMash.Erm.Hrm.Salary.HrmSalaryTaskMatrixReduction);
+            this.BringingMatrixInReducAction.TargetViewId = null;
+            this.BringingMatrixInReducAction.TargetViewType = DevExpress.ExpressApp.ViewType.DetailView;
+            this.BringingMatrixInReducAction.ToolTip = null;
+            this.BringingMatrixInReducAction.TypeOfView = typeof(DevExpress.ExpressApp.DetailView);
+            this.BringingMatrixInReducAction.Execute += new DevExpress.ExpressApp.Actions.SingleChoiceActionExecuteEventHandler(this.BringingMatrixInReduc_Execute);
             // 
-            // AcceptCoercedMatrix
+            // AcceptCoercedMatrixAction
             // 
-            this.AcceptCoercedMatrix.Caption = "Accept MATRIX_COERCED TYPE_MATIX";
-            this.AcceptCoercedMatrix.ConfirmationMessage = null;
-            this.AcceptCoercedMatrix.Id = "AcceptCoercedMatrix";
-            this.AcceptCoercedMatrix.ImageName = null;
-            choiceActionItem4.Caption = "AcceptMinimizeDeviationsMethod";
-            choiceActionItem4.ImageName = null;
-            choiceActionItem4.Shortcut = null;
-            choiceActionItem4.ToolTip = null;
-            choiceActionItem5.Caption = "AcceptMinimizeNumberOfDeviationsMethod";
-            choiceActionItem5.ImageName = null;
-            choiceActionItem5.Shortcut = null;
-            choiceActionItem5.ToolTip = null;
-            choiceActionItem6.Caption = "AcceptProportionsMethod";
-            choiceActionItem6.ImageName = null;
-            choiceActionItem6.Shortcut = null;
-            choiceActionItem6.ToolTip = null;
-            this.AcceptCoercedMatrix.Items.Add(choiceActionItem4);
-            this.AcceptCoercedMatrix.Items.Add(choiceActionItem5);
-            this.AcceptCoercedMatrix.Items.Add(choiceActionItem6);
-            this.AcceptCoercedMatrix.ItemType = DevExpress.ExpressApp.Actions.SingleChoiceActionItemType.ItemIsOperation;
-            this.AcceptCoercedMatrix.Shortcut = null;
-            this.AcceptCoercedMatrix.ShowItemsOnClick = true;
-            this.AcceptCoercedMatrix.Tag = null;
-            this.AcceptCoercedMatrix.TargetObjectsCriteria = null;
-            this.AcceptCoercedMatrix.TargetObjectType = typeof(NpoMash.Erm.Hrm.Salary.HrmSalaryTaskMatrixReduction);
-            this.AcceptCoercedMatrix.TargetViewId = null;
-            this.AcceptCoercedMatrix.TargetViewType = DevExpress.ExpressApp.ViewType.DetailView;
-            this.AcceptCoercedMatrix.ToolTip = null;
-            this.AcceptCoercedMatrix.TypeOfView = typeof(DevExpress.ExpressApp.DetailView);
-            this.AcceptCoercedMatrix.Execute += new DevExpress.ExpressApp.Actions.SingleChoiceActionExecuteEventHandler(this.AcceptCoercedMatrix_Execute);
+            this.AcceptCoercedMatrixAction.Caption = "Accept MATRIX_COERCED TYPE_MATIX";
+            this.AcceptCoercedMatrixAction.ConfirmationMessage = null;
+            this.AcceptCoercedMatrixAction.Id = "HrmSalaryTaskMatrixReductionVC_AcceptCoercedMatrixAction";
+            this.AcceptCoercedMatrixAction.ImageName = null;
+            this.AcceptCoercedMatrixAction.ItemType = DevExpress.ExpressApp.Actions.SingleChoiceActionItemType.ItemIsOperation;
+            this.AcceptCoercedMatrixAction.Shortcut = null;
+            this.AcceptCoercedMatrixAction.ShowItemsOnClick = true;
+            this.AcceptCoercedMatrixAction.Tag = null;
+            this.AcceptCoercedMatrixAction.TargetObjectsCriteria = null;
+            this.AcceptCoercedMatrixAction.TargetObjectType = typeof(NpoMash.Erm.Hrm.Salary.HrmSalaryTaskMatrixReduction);
+            this.AcceptCoercedMatrixAction.TargetViewId = null;
+            this.AcceptCoercedMatrixAction.TargetViewType = DevExpress.ExpressApp.ViewType.DetailView;
+            this.AcceptCoercedMatrixAction.ToolTip = null;
+            this.AcceptCoercedMatrixAction.TypeOfView = typeof(DevExpress.ExpressApp.DetailView);
+            this.AcceptCoercedMatrixAction.Execute += new DevExpress.ExpressApp.Actions.SingleChoiceActionExecuteEventHandler(this.AcceptCoercedMatrixAction_Execute);
             // 
             // ExportCoercedMatrix
             // 
@@ -114,20 +78,19 @@
             this.ExportCoercedMatrix.TargetViewType = DevExpress.ExpressApp.ViewType.DetailView;
             this.ExportCoercedMatrix.ToolTip = null;
             this.ExportCoercedMatrix.TypeOfView = typeof(DevExpress.ExpressApp.DetailView);
-            this.ExportCoercedMatrix.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.ExportCoercedMatrix_Execute);
+            this.ExportCoercedMatrix.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.ExportCoercedMatrixAction_Execute);
             // 
             // HrmSalaryTaskMatrixReductionVC
             // 
             this.TargetObjectType = typeof(NpoMash.Erm.Hrm.Salary.HrmSalaryTaskMatrixReduction);
-            this.TargetViewType = DevExpress.ExpressApp.ViewType.DetailView;
-            this.TypeOfView = typeof(DevExpress.ExpressApp.DetailView);
+            this.TypeOfView = typeof(DevExpress.ExpressApp.View);
 
         }
 
         #endregion
 
-        private DevExpress.ExpressApp.Actions.SingleChoiceAction BringingMatrixInReduc;
-        private DevExpress.ExpressApp.Actions.SingleChoiceAction AcceptCoercedMatrix;
+        private DevExpress.ExpressApp.Actions.SingleChoiceAction BringingMatrixInReducAction;
+        private DevExpress.ExpressApp.Actions.SingleChoiceAction AcceptCoercedMatrixAction;
         private DevExpress.ExpressApp.Actions.SimpleAction ExportCoercedMatrix;
     }
 }
--- trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/Salary/HrmSalaryTaskMatrixReductionVC.cs
+++ trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/Salary/HrmSalaryTaskMatrixReductionVC.cs
@@ -24,47 +24,116 @@ namespace NpoMash.Erm.Hrm.Salary {
             InitializeComponent();
             RegisterActions(components);
             // Target required Views (via the TargetXXX properties) and create their Actions.
+            //
+            // Заполним список Items на основе enum HrmMatrixVariant
+            foreach (object current in Enum.GetValues(typeof(HrmMatrixVariant))) {
+                EnumDescriptor ed = new EnumDescriptor(typeof(HrmMatrixVariant));
+                ChoiceActionItem item = new ChoiceActionItem(ed.GetCaption(current), current);
+                BringingMatrixInReducAction.Items.Add(item);
+                item = new ChoiceActionItem(ed.GetCaption(current), current);
+                AcceptCoercedMatrixAction.Items.Add(item);
+            }
         }
         protected override void OnActivated() {
             base.OnActivated();
             // Perform various tasks depending on the target View.
+            UpdateActionsItemsState();
+            View.CurrentObjectChanged += new EventHandler(View_CurrentObjectChanged);
+        }
+
+        void View_CurrentObjectChanged(object sender, EventArgs e) {
+            UpdateActionsItemsState();
         }
         protected override void OnViewControlsCreated() {
             base.OnViewControlsCreated();
             // Access and customize the target View control.
         }
         protected override void OnDeactivated() {
+            View.CurrentObjectChanged -= new EventHandler(View_CurrentObjectChanged);
             // Unsubscribe from previously subscribed events and release other references and resources.
             base.OnDeactivated();
         }
 
+        public void UpdateActionsItemsState() {
+            HrmSalaryTaskMatrixReduction task = (HrmSalaryTaskMatrixReduction) View.CurrentObject as HrmSalaryTaskMatrixReduction;
+            if (task == null)
+                return;
+            foreach (ChoiceActionItem  choice_item in BringingMatrixInReducAction.Items) {
+                SetItemState(choice_item, task, false);
+            }
+            foreach (ChoiceActionItem choice_item in AcceptCoercedMatrixAction.Items) {
+                SetItemState(choice_item, task, true);
+            }
+        }
+
+        private void SetItemState(ChoiceActionItem choice_item, HrmSalaryTaskMatrixReduction task, Boolean value) {
+            HrmMatrixVariant variant = (HrmMatrixVariant)choice_item.Data;
+            switch (variant) {
+                case HrmMatrixVariant.MINIMIZE_MAXIMUM_DEVIATIONS_VARIANT:
+                    if (task.MinimizeMaximumDeviationsMatrix != null)
+                        choice_item.Active.SetItemValue(this.GetType().FullName, value);
+                    else
+                        choice_item.Active.SetItemValue(this.GetType().FullName, !value);
+                    break;
+                case HrmMatrixVariant.MINIMIZE_NUMBER_OF_DEVIATIONS_VARIANT:
+                    if (task.MinimizeNumberOfDeviationsMatrix != null)
+                        choice_item.Active.SetItemValue(this.GetType().FullName, value);
+                    else
+                        choice_item.Active.SetItemValue(this.GetType().FullName, !value);
+                    break;
+                case HrmMatrixVariant.PROPORTIONS_METHOD_VARIANT:
+                    if (task.ProportionsMethodMatrix != null)
+                        choice_item.Active.SetItemValue(this.GetType().FullName, value);
+                    else
+                        choice_item.Active.SetItemValue(this.GetType().FullName, !value);
+                    break;
+            }
+        }
 
         private void BringingMatrixInReduc_Execute(object sender, SingleChoiceActionExecuteEventArgs e) {
-            IObjectSpace os = ObjectSpace;
-            HrmSalaryTaskMatrixReduction reduc = (HrmSalaryTaskMatrixReduction)e.CurrentObject;
-            HrmPeriod period = os.GetObject<HrmPeriod>(reduc.Period);
-            if (period.Status == HrmPeriodStatus.READY_TO_CALCULATE_COERCED_MATRIXS) {
-                HrmMatrixVariant bringing_method = HrmSalaryTaskMatrixReductionLogic.DetermineSelectedBringingMethod(e);
-                HrmSalaryTaskMatrixReductionLogic.CreateMatrixInReduc(reduc, os, reduc.GroupDep, bringing_method, period);
-            }
-        }
-
-        private void AcceptCoercedMatrix_Execute(object sender, SingleChoiceActionExecuteEventArgs e) {
-            IObjectSpace os = ObjectSpace;
-            HrmSalaryTaskMatrixReduction reduc = (HrmSalaryTaskMatrixReduction)e.CurrentObject;
-            HrmPeriod current_period = os.GetObject<HrmPeriod>(reduc.Period);
-            //HrmSalaryTaskMatrixReduction reduc = os.GetObject<HrmSalaryTaskMatrixReduction>(red);
-            HrmMatrix matrix_to_accept = HrmSalaryTaskMatrixReductionLogic.DetermineSelectedMatrixToAccept(e, reduc);            
-            if (matrix_to_accept != null && matrix_to_accept.Status == HrmMatrixStatus.MATRIX_SAVED) {
-                HrmSalaryTaskMatrixReductionLogic.AcceptSelectedMatrix(reduc, matrix_to_accept);
-                if (HrmSalaryTaskMatrixReductionLogic.AllCoercedMatrixesAccepted(matrix_to_accept, current_period))
-                    current_period.setStatus(HrmPeriodStatus.READY_TO_EXPORT_CORCED_MATRIXS);
+            HrmSalaryTaskMatrixReduction task = (HrmSalaryTaskMatrixReduction)e.CurrentObject;
+            using (IObjectSpace os = ObjectSpace.CreateNestedObjectSpace()) {
+                task = os.GetObject<HrmSalaryTaskMatrixReduction>(task);
+                if (task.Period.Status == HrmPeriodStatus.READY_TO_CALCULATE_COERCED_MATRIXS) {
+                    //                HrmMatrixVariant bringing_method = HrmSalaryTaskMatrixReductionLogic.DetermineSelectedBringingMethod(e);
+                    HrmMatrixVariant bringing_method = (HrmMatrixVariant)e.SelectedChoiceActionItem.Data;
+                    HrmSalaryTaskMatrixReductionLogic.CreateMatrixInReduc(task, os, task.GroupDep, bringing_method, task.Period);
+                }
                 os.CommitChanges();
             }
-
+            UpdateActionsItemsState();
         }
 
-        private void ExportCoercedMatrix_Execute(object sender, SimpleActionExecuteEventArgs e) {
+        private void AcceptCoercedMatrixAction_Execute(object sender, SingleChoiceActionExecuteEventArgs e) {
+            HrmSalaryTaskMatrixReduction task = (HrmSalaryTaskMatrixReduction)e.CurrentObject;
+            using (IObjectSpace os = ObjectSpace.CreateNestedObjectSpace()) {
+                task = os.GetObject<HrmSalaryTaskMatrixReduction>(task);
+                HrmMatrixVariant bringing_method = (HrmMatrixVariant)e.SelectedChoiceActionItem.Data;
+                HrmMatrix matrix_to_accept = null;
+                switch (bringing_method) {
+                    case HrmMatrixVariant.MINIMIZE_MAXIMUM_DEVIATIONS_VARIANT:
+                        matrix_to_accept = task.MinimizeMaximumDeviationsMatrix;
+                        break;
+                    case HrmMatrixVariant.MINIMIZE_NUMBER_OF_DEVIATIONS_VARIANT:
+                        matrix_to_accept = task.MinimizeNumberOfDeviationsMatrix;
+                        break;
+                    case HrmMatrixVariant.PROPORTIONS_METHOD_VARIANT:
+                        matrix_to_accept = task.ProportionsMethodMatrix;
+                        break;
+                }
+                if (matrix_to_accept != null && matrix_to_accept.Status == HrmMatrixStatus.MATRIX_SAVED) {
+                    HrmSalaryTaskMatrixReductionLogic.AcceptSelectedMatrix(task, matrix_to_accept);
+                    if (HrmSalaryTaskMatrixReductionLogic.AllCoercedMatrixesAccepted(matrix_to_accept, task.Period))
+                        task.Period.setStatus(HrmPeriodStatus.READY_TO_EXPORT_CORCED_MATRIXS);
+                }
+                task.Complete();
+                os.CommitChanges();
+            }
+            ObjectSpace.CommitChanges();
+            UpdateActionsItemsState();
+        }
+
+        private void ExportCoercedMatrixAction_Execute(object sender, SimpleActionExecuteEventArgs e) {
             HrmSalaryTaskMatrixReduction reduc = (HrmSalaryTaskMatrixReduction)e.CurrentObject;
             IObjectSpace os = ObjectSpace;
             HrmPeriod current_period = os.GetObject<HrmPeriod>(reduc.Period);
