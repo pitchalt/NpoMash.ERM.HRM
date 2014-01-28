bb71539fa11aa2b33e7f8dbcfe2a336637a0952f
DeminD002 <DeminD002@294e8926-5120-4385-9bc8-594b46af50e8>
2014-01-28 18:55:16 +0000

    Теперь класс тестовой логии более гибкий, а так же добавлена начальная логика тестирования приведения

---
+++ trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/StructuralTests/BringingLogicTests/OptionsBringingLogic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Xpo;
+
+using NpoMash.Erm.Hrm.Salary;
+using NpoMash.Erm.Hrm.Tests.Controllers;
+
+using NUnit.Framework;
+
+namespace NpoMash.Erm.Hrm.Tests.StructuralTests.BringingLogicTests {
+
+
+    [TestFixture]
+    public class OptionsBringingLogic {
+
+        protected TestApplication application;
+
+        [TestFixtureSetUp]
+        protected virtual void SetUp() {
+            IObjectSpaceProvider object_space_provider = new XPObjectSpaceProvider(new MemoryDataStoreProvider());
+            application = new TestApplication();
+            ModuleBase test_module = new ModuleBase();
+            test_module.AdditionalExportedTypes.Add(typeof(HrmSalaryTaskMatrixReduction));
+            application.Modules.Add(test_module);
+            application.Setup("BringingApp", object_space_provider);
+            IObjectSpace object_space = application.CreateObjectSpace();
+        }
+
+
+
+    }
+}
