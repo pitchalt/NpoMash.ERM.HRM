e5a0dfe0ac3670c161874ad284cfa437c95e36d1
KovtD003 <KovtD003@294e8926-5120-4385-9bc8-594b46af50e8>
2014-01-29 20:33:53 +0000

    убрали лишние рули

--- trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/Salary/HrmMatrixCell.cs
+++ trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/Salary/HrmMatrixCell.cs
@@ -27,7 +27,7 @@ namespace NpoMash.Erm.Hrm.Salary {
 
         private Decimal _Sum;
         [ModelDefault("DisplayFormat", "{0:N}")]
-        [RuleValueComparison(null, DefaultContexts.Save, ValueComparisonType.GreaterThan, 0)]
+        [RuleValueComparison(null, DefaultContexts.Save, ValueComparisonType.GreaterThanOrEqual, 0)]
         public Decimal Sum {
             get { return _Sum; }
             set { SetPropertyValue<Decimal>("Sum", ref _Sum, value); }
