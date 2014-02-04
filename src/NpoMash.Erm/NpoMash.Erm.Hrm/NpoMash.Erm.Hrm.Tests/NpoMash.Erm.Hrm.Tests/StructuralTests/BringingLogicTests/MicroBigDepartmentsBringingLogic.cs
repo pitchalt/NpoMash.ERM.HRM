d6851043a76d4394343a6b7c62ded678c9a458bd
DeminD002 <DeminD002@294e8926-5120-4385-9bc8-594b46af50e8>
2014-02-04 19:18:03 +0000

    Еще порция тестов

--- src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/StructuralTests/BringingLogicTests/MicroBigDepartmentsBringingLogic.cs
+++ src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/StructuralTests/BringingLogicTests/MicroBigDepartmentsBringingLogic.cs
@@ -36,5 +36,17 @@ namespace NpoMash.Erm.Hrm.Tests.StructuralTests.BringingLogicTests {
                 Assert.AreEqual(department.plan, department.fact);
             }
         }
+
+        [Test]
+        public void BringMicroBringBig_2on3on5Classic50_PlanFactAreEqualExpect() {
+            IObjectSpace test_object_space = application.CreateObjectSpace();
+            Matrix matrix = BringingLogic.PrepareBringingStructure(PrepareTestData(test_object_space, 20, 30, 2, 3, 5, 50));
+            BringingLogic.BringMicroDepartments(matrix);
+            BringingLogic.BringBigDepartments(matrix);
+            BringingLogic.RestoreInitialFact(matrix);
+            foreach (var department in matrix.deps.Values) {
+                Assert.AreEqual(department.plan, department.fact);
+            }
+        }
     }
 }
