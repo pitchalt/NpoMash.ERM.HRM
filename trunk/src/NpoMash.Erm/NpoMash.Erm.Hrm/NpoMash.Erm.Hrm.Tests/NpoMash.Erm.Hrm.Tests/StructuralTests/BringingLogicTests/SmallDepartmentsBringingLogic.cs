1d915b4b5f0b5c3568b24b130903d7b4cbd997e4
DeminD002 <DeminD002@294e8926-5120-4385-9bc8-594b46af50e8>
2014-02-04 17:52:08 +0000

    Окончательная версия генератора, пошли тесты..

---
+++ trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/StructuralTests/BringingLogicTests/SmallDepartmentsBringingLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using DevExpress.ExpressApp;
+
+using NpoMash.Erm.Hrm.Salary;
+using NpoMash.Erm.Hrm.Salary.BringingStructure;
+
+using NUnit.Framework;
+
+namespace NpoMash.Erm.Hrm.Tests.StructuralTests.BringingLogicTests {
+
+
+    [TestFixture]
+    public class SmallDepartmentsBringingLogic : OptionsBringingLogic {
+
+        [Test]
+        public void BringUncontrolledOrders_1SmallDepClassic_PlanFactEqualExpect() {
+            IObjectSpace test_object_space = application.CreateObjectSpace();
+            Matrix matrix = BringingLogic.PrepareBringingStructure(PrepareTestData(test_object_space, 1, 1, 0, 0, 1, 100));
+            BringingLogic.BringUncontrolledOrders(matrix);
+            BringingLogic.RestoreInitialFact(matrix);
+            foreach (var department in matrix.deps.Values) {
+                Assert.AreEqual(department.plan, department.fact);
+            }
+        }
+    }  
+}
