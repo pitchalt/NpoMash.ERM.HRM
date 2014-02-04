03e7292b722ff021056ab4ae33fa61949fa9ab2d
DeminD002 <DeminD002@294e8926-5120-4385-9bc8-594b46af50e8>
2014-02-04 18:55:45 +0000

    Найдена ошибка - индекс вне диапазона, зацикливание

---
+++ src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/StructuralTests/BringingLogicTests/MicroBigDepartmentsBringingLogic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using DevExpress.ExpressApp;
+
+using NpoMash.Erm.Hrm.Salary;
+using NpoMash.Erm.Hrm.Salary.BringingStructure;
+
+using NUnit.Framework;
+
+namespace NpoMash.Erm.Hrm.Tests.StructuralTests.BringingLogicTests {
+
+
+    [TestFixture]
+    public class MicroBigDepartmentsBringingLogic : OptionsBringingLogic {
+
+        [Test]
+        public void BringMicroBringBig_1on1Classic_PlanFactAreEqualExpect() {
+            IObjectSpace test_object_space = application.CreateObjectSpace();
+            Matrix matrix = BringingLogic.PrepareBringingStructure(PrepareTestData(test_object_space, 2, 2, 1, 1, 0, 100));
+            BringingLogic.BringMicroDepartments(matrix);
+            BringingLogic.BringBigDepartments(matrix);
+            BringingLogic.RestoreInitialFact(matrix);
+            foreach (var department in matrix.deps.Values) {
+                Assert.AreEqual(department.plan, department.fact);
+            }
+        }
+    }
+}
