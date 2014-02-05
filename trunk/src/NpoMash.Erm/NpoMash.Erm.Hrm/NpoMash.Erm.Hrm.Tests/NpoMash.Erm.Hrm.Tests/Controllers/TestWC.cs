c5f39ea5ae48ea13625d43ddac465130a4f24094
DeminD002 <DeminD002@294e8926-5120-4385-9bc8-594b46af50e8>
2014-02-05 21:37:48 +0000

    Добавил костыль с импортом справок

--- trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/Controllers/TestWC.cs
+++ trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/Controllers/TestWC.cs
@@ -36,7 +36,7 @@ namespace NpoMash.Erm.Hrm.Tests.Controllers {
 
         private void AddReferenceData_Execute(object sender, SimpleActionExecuteEventArgs e) {
             IObjectSpace object_space = Application.CreateObjectSpace();
-            TestWCLogic.ImportDeps(object_space);
+            TestWCLogic.ImportDepartments(object_space);
             TestWCLogic.ImportOrders(object_space);
             TestWCLogic.SalaryPayTypeGenerate(object_space);
             TestWCLogic.addTestData(object_space);
