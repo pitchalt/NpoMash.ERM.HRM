fb242c17ac76b89256cc45a7fef1d2597632b616
Student001 <Student001@294e8926-5120-4385-9bc8-594b46af50e8>
2014-02-09 12:33:49 +0000

    -Заглушки

---
+++ trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/Salary/HrmSalaryTaskProvisionMatrixReductionVC.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+//
+using DevExpress.ExpressApp;
+using DevExpress.Data.Filtering;
+using DevExpress.Persistent.Base;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.Persistent.Validation;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+//
+
+namespace NpoMash.Erm.Hrm.Salary {
+    public partial class HrmSalaryTaskProvisionMatrixReductionVC : ViewController {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public HrmSalaryTaskProvisionMatrixReductionVC() { InitializeComponent(); RegisterActions(components); }
+        protected override void OnActivated() {
+            base.OnActivated();
+        }
+        protected override void OnViewControlsCreated() {
+            base.OnViewControlsCreated();
+        }
+        protected override void OnDeactivated() {
+            base.OnDeactivated();
+        }
+    }
+}
