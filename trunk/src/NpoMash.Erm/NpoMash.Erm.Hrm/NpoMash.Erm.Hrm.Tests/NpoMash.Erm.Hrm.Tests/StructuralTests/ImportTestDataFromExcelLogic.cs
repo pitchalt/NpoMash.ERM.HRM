a6df060e5c25fcdf0e3776327e63a27b45fdb108
KovtD003 <KovtD003@294e8926-5120-4385-9bc8-594b46af50e8>
2014-03-10 21:26:41 +0000

    ночной коммит с улучшенным екселем

--- trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/StructuralTests/ImportTestDataFromExcelLogic.cs
+++ trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/StructuralTests/ImportTestDataFromExcelLogic.cs
@@ -10,19 +10,13 @@ using DevExpress.Office;
 
 
 namespace NpoMash.Erm.Hrm.Tests.StructuralTests {
-    struct CellImportedFormExcel {
-        public List<CellValue> cell_info;
-        public List<CellValue> row_info;
-        public List<CellValue> col_info;
-        public List<CellValue> itog_row_info;
-        public List<CellValue> itog_col_info;
-        }
+
 
     static class ImportTestDataFromExcelLogic {
 
-        static List<CellImportedFormExcel> GetData(String path_to_file, int itogs_in_row = 1, int itogs_in_col = 1) {
+        static MatrixFromExcel GetData(String path_to_file, int itogs_in_row = 1, int itogs_in_col = 1) {
             const String end_string = "Итого";
-            List<CellImportedFormExcel> result = new List<CellImportedFormExcel>();
+            //List<CellImportedFormExcel> result = new List<CellImportedFormExcel>();
             Workbook wb = new Workbook();
             wb.LoadDocument(path_to_file, DocumentFormat.Xls);
             Worksheet ws = wb.Worksheets.First();
@@ -43,47 +37,61 @@ namespace NpoMash.Erm.Hrm.Tests.StructuralTests {
                 throw new Exception("The input file is invalid");
             int current_row = data_angle_row;
             int current_col = data_angle_col;
+            MatrixFromExcel result = new MatrixFromExcel(end_row - data_angle_row, end_col - data_angle_col);
+            int index_row = 0;
             while (ws.Cells[current_row, 0].Value != end_string) {
                 int row_merge_size = ws.Cells[current_row, 0].GetMergedRanges().Count();
-
+                int index_col = 0;
                 while (ws.Cells[0, current_col].Value != end_string) {
                     int col_merge_size = ws.Cells[0, current_col].GetMergedRanges().Count();
-                    List<CellValue> cell_info = new List<CellValue>();
+                    List<Decimal> cell_info = new List<Decimal>();
                     int current_cell_col = current_col;
                     int current_cell_row = current_row;
                     bool is_empty_cells = false;
+                    bool all_cells_are_empty = true;
                     while (current_cell_row < current_row + row_merge_size && !is_empty_cells) {
                         while (current_cell_col < current_col + col_merge_size && !is_empty_cells) {
                             CellValue cv = ws.Cells[current_cell_row, current_cell_col].Value;
                             if (cv.IsEmpty)
                                 is_empty_cells = true;
-                            else cell_info.Add(cv);
+                            else {
+                                cell_info.Add((decimal)cv.NumericValue);
+                                all_cells_are_empty = false;
+                            }
                             current_cell_row++;
                         }
                         current_cell_col++;
                     }
-                    List<CellValue> col_info = new List<CellValue>();
-                    for (int i = 0; i < data_angle_row; i++)
-                        col_info.Add(ws.Cells[i, current_col].Value);
-                    List<CellValue> row_info = new List<CellValue>();
-                    for (int i = 0; i < data_angle_col; i++)
-                        row_info.Add(ws.Cells[current_row, i].Value);
-                    current_col += col_merge_size;
-                    List<CellValue> itog_col_info = new List<CellValue>();
-                    for (int i = 0; i < itogs_in_col; i++)
-                        itog_col_info.Add(ws.Cells[end_row + i, current_col].Value);
-                    List<CellValue> itog_row_info = new List<CellValue>();
-                    for (int i = 0; i < itogs_in_row; i++)
-                        itog_row_info.Add(ws.Cells[current_row, end_col + i].Value);
-                    CellImportedFormExcel c = new CellImportedFormExcel();
-                    c.cell_info = cell_info;
-                    c.col_info = col_info;
-                    c.row_info = row_info;
-                    c.itog_col_info = itog_col_info;
-                    c.itog_row_info = itog_row_info;
-                    result.Add(c);
+                    if (!all_cells_are_empty) {
+                        if (result.columns_info[index_col] == null) {
+                            List<CellValue> col_info = new List<CellValue>();
+                            for (int i = 0; i < data_angle_row; i++)
+                                col_info.Add(ws.Cells[i, current_col].Value);
+                            List<CellValue> itog_col_info = new List<CellValue>();
+                            for (int i = 0; i < itogs_in_col; i++)
+                                itog_col_info.Add(ws.Cells[end_row + i, current_col].Value);
+                            result.columns_info[index_col] = col_info;
+                            result.itog_columns_info[index_col] = col_info;
+                        }
+
+                        if (result.rows_info[index_row] == null) {
+                            List<CellValue> row_info = new List<CellValue>();
+                            for (int i = 0; i < data_angle_col; i++)
+                                row_info.Add(ws.Cells[current_row, i].Value);
+                            current_col += col_merge_size;
+                            List<CellValue> itog_row_info = new List<CellValue>();
+                            for (int i = 0; i < itogs_in_row; i++)
+                                itog_row_info.Add(ws.Cells[current_row, end_col + i].Value);
+
+                            result.rows_info[index_row] = row_info;
+                            result.itog_rows_info[index_row] = itog_row_info;
+                        }
+                        result.mat[index_row, index_col] = cell_info;
+                    }
+                    index_col++;
                 }
                 current_row += row_merge_size;
+                index_row++;
             }
             return result;
         }
