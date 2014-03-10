e61b2144f99ae38a4ab813e604e464ddd9d23c71
KovtD003 <KovtD003@294e8926-5120-4385-9bc8-594b46af50e8>
2014-03-10 21:26:41 +0000

    ночной коммит с улучшенным екселем

---
+++ src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/StructuralTests/ImportTestDataFromExcelStructure.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DevExpress;
+using DevExpress.Spreadsheet;
+using DevExpress.Office;
+
+namespace NpoMash.Erm.Hrm.Tests.StructuralTests {
+    class MatrixFromExcel {
+        public List<Decimal>[,] mat;
+        public List<CellValue>[] rows_info;
+        public List<CellValue>[] columns_info;
+        public List<CellValue>[] itog_columns_info;
+        public List<CellValue>[] itog_rows_info;
+        private int _NumberOfRows;
+        public int NumberOfRows{get{return _NumberOfRows;}}
+        private int _NumberOfColumns;
+        public int NumberOfColumns { get { return _NumberOfColumns; } }        
+
+        public MatrixFromExcel(int rows_number, int columns_number) {
+            _NumberOfColumns = columns_number;
+            _NumberOfRows = rows_number;
+            mat = new List<decimal>[rows_number, columns_number];
+            rows_info = new List<CellValue>[rows_number];
+            columns_info = new List<CellValue>[columns_number];
+            itog_columns_info = new List<CellValue>[columns_number];
+            itog_rows_info = new List<CellValue>[rows_number];
+        }
+
+    }
+
+}
