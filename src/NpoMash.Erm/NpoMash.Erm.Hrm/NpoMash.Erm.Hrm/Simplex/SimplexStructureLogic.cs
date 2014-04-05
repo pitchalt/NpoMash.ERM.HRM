039000e806ad85caaa083ea788797874f42b962e
KovtD003 <KovtD003@294e8926-5120-4385-9bc8-594b46af50e8>
2014-04-05 00:47:44 +0000

    ядро симплекса, рабочее

--- src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/Simplex/SimplexStructureLogic.cs
+++ src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/Simplex/SimplexStructureLogic.cs
@@ -5,6 +5,30 @@ using System.Text;
 
 namespace NpoMash.Erm.Hrm.Simplex {
     class SimplexStructureLogic {
+        public static double[] Maximize(SimplexTab tab) {
+            int guiding_column = tab.ColumnWithMinDelta();
+            while (tab.delta[guiding_column] < 0) {
+                int guiding_row = tab.FindGuidingRow(guiding_column);
+                tab.ConvertToNextTab(guiding_row, guiding_column);
+                guiding_column = tab.ColumnWithMinDelta();
+            }
+            double[] result = new double[tab.maxIndexOfUnfictiveVar + 1];
+            for (int i = 0; i < tab.numberOfRows; i++)
+                result[tab.basis[i]] = tab.bearingPlan[i];
+            return result;
+        }
 
+        public static double[] Minimize(SimplexTab tab) {
+            int guiding_column = tab.ColumnWithMaxDelta();
+            while (tab.delta[guiding_column] > 0) {
+                int guiding_row = tab.FindGuidingRow(guiding_column);
+                tab.ConvertToNextTab(guiding_row, guiding_column);
+                guiding_column = tab.ColumnWithMaxDelta();
+            }
+            double[] result = new double[tab.maxIndexOfUnfictiveVar + 1];
+            for (int i = 0; i < tab.numberOfRows; i++)
+                result[tab.basis[i]] = tab.bearingPlan[i];
+            return result;
+        }
     }
 }
