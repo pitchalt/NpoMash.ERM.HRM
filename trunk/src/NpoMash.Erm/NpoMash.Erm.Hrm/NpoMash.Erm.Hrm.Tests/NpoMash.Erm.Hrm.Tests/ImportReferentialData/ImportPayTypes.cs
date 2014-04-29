7f0a7363c1c5dbd478d3e069bb17e4563b6891c4
DeminD002 <DeminD002@294e8926-5120-4385-9bc8-594b46af50e8>
2014-04-29 21:29:28 +0000

    В этой ревизии добавлен импорт справочника кодов оплат в формате cnd

--- trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/ImportReferentialData/ImportPayTypes.cs
+++ trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests/ImportReferentialData/ImportPayTypes.cs
@@ -4,12 +4,28 @@ using FileHelpers;
 namespace NpoMash.Erm.Hrm.Tests.ImportReferentialData {
     
     
-    [FixedLengthRecord(FixedMode.AllowLessChars)]
+    [FixedLengthRecord()]
     public class ImportPayTypes {
  
         [FieldFixedLength(3)]
         [FieldConverter(typeof(PayTypeConverter))]
-        public String PayTypeCode;
+        public String Code;
+
+        [FieldFixedLength(7)]
+        [FieldConverter(typeof(PayTypeConverter))]
+        public String From;
+
+        [FieldFixedLength(7)]
+        [FieldConverter(typeof(PayTypeConverter))]
+        public String To;
+
+        [FieldFixedLength(12)]
+        [FieldConverter(typeof(PayTypeConverter))]
+        public String ShortName;
+
+        [FieldFixedLength(40)]
+        [FieldConverter(typeof(PayTypeConverter))]
+        public String Name;
 
         internal class PayTypeConverter : ConverterBase {
             public override object StringToField(string from) {
