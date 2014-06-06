f1d1e11e213ee851b3168d20020693cea332da6f
Student001 <Student001@294e8926-5120-4385-9bc8-594b46af50e8>
2014-06-06 10:54:06 +0000

    -Теперь план и матрица учета в карточках совпадают

--- trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/Salary/Tasks/HrmSalaryTaskCompareWorkTimeLogic.cs
+++ trunk/src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm/Salary/Tasks/HrmSalaryTaskCompareWorkTimeLogic.cs
@@ -29,44 +29,53 @@ namespace NpoMash.Erm.Hrm.Salary {
         public static void CompareKBMatrix(IObjectSpace object_space, HrmSalaryTaskCompareWorkTime task) {
             task.AllocResultKB = task.Period.CurrentMatrixAllocResultKB;
             task.CurrentTimeSheetKB = task.Period.CurrentTimeSheetKB;
+            task.MinimizeNumberOfDeviationsMatrix = task.Period.CurrentKBmatrixReduction.MinimizeNumberOfDeviationsMatrix;
+   
             //Вытаскиваем плановую матрицу
             foreach (HrmMatrix matrix in task.Period.Matrixs) {
                 if (matrix.GroupDep == DepartmentGroupDep.DEPARTMENT_KB && matrix.Type == HrmMatrixType.TYPE_MATIX &&
                     matrix.Status == HrmMatrixStatus.MATRIX_ACCEPTED && matrix.TypeMatrix == HrmMatrixTypeMatrix.MATRIX_PLANNED) {
                     task.MatrixPlan = matrix;
                 }
-            }
-
-            foreach (HrmMatrix matrix in task.Period.Matrixs) {
-                if (matrix.GroupDep == DepartmentGroupDep.DEPARTMENT_KB && matrix.Type == HrmMatrixType.TYPE_MATIX &&
-                    matrix.Status == HrmMatrixStatus.MATRIX_EXPORTED && matrix.TypeMatrix == HrmMatrixTypeMatrix.MATRIX_COERCED) {
-                    task.MinimizeNumberOfDeviationsMatrix = matrix;
                 }
-            }
+
 
             task.AllocResultKB.GroupDep = DepartmentGroupDep.DEPARTMENT_KB;
             task.AllocResultKB.Status = HrmMatrixStatus.MATRIX_ACCEPTED;
             task.AllocResultKB.Type = HrmMatrixType.TYPE_ALLOC_RESULT;
             task.GroupDep = DepartmentGroupDep.DEPARTMENT_KB;
-        }
+            }
+
+          /*  foreach (HrmMatrix matrix in task.Period.Matrixs) {
+                if (matrix.GroupDep == DepartmentGroupDep.DEPARTMENT_KB && matrix.Type == HrmMatrixType.TYPE_MATIX &&
+                    matrix.Status == HrmMatrixStatus.MATRIX_EXPORTED && matrix.TypeMatrix == HrmMatrixTypeMatrix.MATRIX_COERCED) {
+                    task.MinimizeNumberOfDeviationsMatrix = matrix;
+                }
+            }*/
+
+
+        
 
         public static void CompareOZMMatrix(IObjectSpace object_space, HrmSalaryTaskCompareWorkTime task) {
             task.AllocResultOZM = task.Period.CurrentMatrixAllocResultOZM;
             task.CurrentTimeSheetOZM = task.Period.CurrentTimeSheetOZM;
+            task.MinimizeNumberOfDeviationsMatrix = task.Period.CurrentOZMmatrixReduction.MinimizeNumberOfDeviationsMatrix;
             //Вытаскиваем плановую матрицу
-            foreach (HrmMatrix matrix in task.Period.Matrixs) {
+         /*   foreach (HrmMatrix matrix in task.Period.Matrixs) {
                 if (matrix.GroupDep == DepartmentGroupDep.DEPARTMENT_OZM && matrix.Type == HrmMatrixType.TYPE_MATIX &&
                     matrix.Status == HrmMatrixStatus.MATRIX_ACCEPTED && matrix.TypeMatrix == HrmMatrixTypeMatrix.MATRIX_PLANNED) {
                     task.MatrixPlan = matrix;
                 }
             }
-
-            foreach (HrmMatrix matrix in task.Period.Matrixs) {
+            */
+          /*  foreach (HrmMatrix matrix in task.Period.Matrixs) {
                 if (matrix.GroupDep == DepartmentGroupDep.DEPARTMENT_OZM && matrix.Type == HrmMatrixType.TYPE_MATIX &&
                     matrix.Status == HrmMatrixStatus.MATRIX_EXPORTED && matrix.TypeMatrix == HrmMatrixTypeMatrix.MATRIX_COERCED) {
                     task.MinimizeNumberOfDeviationsMatrix = matrix;
                 }
-            }
+            }*/
+
+
             task.GroupDep = DepartmentGroupDep.DEPARTMENT_OZM;
             task.AllocResultOZM.GroupDep = DepartmentGroupDep.DEPARTMENT_OZM;
             task.AllocResultOZM.Status = HrmMatrixStatus.MATRIX_ACCEPTED;
