0622a9272e421dfda6c8eadb29cc4e640731a4d6
pitchalt <pitchalt@294e8926-5120-4385-9bc8-594b46af50e8>
2014-06-12 20:40:01 +0000

    Обновил структуру проекта, теперь основной проект это NpoMash.Erm.Hrm.Salary.AppWin тестовые модули попробуем отключить в продакшене

---
+++ src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Salary/NpoMash.Erm.Hrm.Salary.AppWin/WinApplication.Designer.cs
@@ -0,0 +1,145 @@
+namespace NpoMash.Erm.Hrm.Salary.Win {
+    partial class NpoErmHrmSalaryWinApplication {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule();
+            this.module3 = new NpoMash.Erm.Hrm.Salary.NpoErmHrmSalaryModule();
+            this.module4 = new NpoMash.Erm.Hrm.Salary.Win.NpoErmHrmSalaryWinModule();
+            this.conditionalAppearanceModule1 = new DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule();
+            this.validationModule1 = new DevExpress.ExpressApp.Validation.ValidationModule();
+            this.iagErmModule1 = new IntecoAG.ERM.IagErmModule();
+            this.iagXafExtModule1 = new IntecoAG.XafExt.IagXafExtModule();
+            this.cloneObjectModule1 = new DevExpress.ExpressApp.CloneObject.CloneObjectModule();
+            this.viewVariantsModule1 = new DevExpress.ExpressApp.ViewVariantsModule.ViewVariantsModule();
+            this.pivotGridModule1 = new DevExpress.ExpressApp.PivotGrid.PivotGridModule();
+            this.reportsModule1 = new DevExpress.ExpressApp.Reports.ReportsModule();
+            this.chartModule1 = new DevExpress.ExpressApp.Chart.ChartModule();
+            this.stateMachineModule1 = new DevExpress.ExpressApp.StateMachine.StateMachineModule();
+            this.npoErmHrmModule1 = new NpoMash.Erm.Hrm.NpoErmHrmModule();
+            this.chartWindowsFormsModule1 = new DevExpress.ExpressApp.Chart.Win.ChartWindowsFormsModule();
+            this.fileAttachmentsWindowsFormsModule1 = new DevExpress.ExpressApp.FileAttachments.Win.FileAttachmentsWindowsFormsModule();
+            this.pivotChartModuleBase1 = new DevExpress.ExpressApp.PivotChart.PivotChartModuleBase();
+            this.pivotChartWindowsFormsModule1 = new DevExpress.ExpressApp.PivotChart.Win.PivotChartWindowsFormsModule();
+            this.pivotGridWindowsFormsModule1 = new DevExpress.ExpressApp.PivotGrid.Win.PivotGridWindowsFormsModule();
+            this.reportsWindowsFormsModule1 = new DevExpress.ExpressApp.Reports.Win.ReportsWindowsFormsModule();
+            this.scriptRecorderModuleBase1 = new DevExpress.ExpressApp.ScriptRecorder.ScriptRecorderModuleBase();
+            this.scriptRecorderWindowsFormsModule1 = new DevExpress.ExpressApp.ScriptRecorder.Win.ScriptRecorderWindowsFormsModule();
+            this.treeListEditorsModuleBase1 = new DevExpress.ExpressApp.TreeListEditors.TreeListEditorsModuleBase();
+            this.treeListEditorsWindowsFormsModule1 = new DevExpress.ExpressApp.TreeListEditors.Win.TreeListEditorsWindowsFormsModule();
+            this.validationWindowsFormsModule1 = new DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule();
+            this.npoErmHrmWinModule1 = new NpoMash.Erm.Hrm.Win.NpoErmHrmWinModule();
+            this.npoErmHrmTestsModule1 = new NpoMash.Erm.Hrm.Tests.NpoErmHrmTestsModule();
+            this.iagXafExtWinModule1 = new IntecoAG.XafExt.Win.IagXafExtWinModule();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // validationModule1
+            // 
+            this.validationModule1.AllowValidationDetailsAccess = true;
+            this.validationModule1.IgnoreWarningAndInformationRules = false;
+            // 
+            // reportsModule1
+            // 
+            this.reportsModule1.EnableInplaceReports = true;
+            this.reportsModule1.ReportDataType = typeof(DevExpress.Persistent.BaseImpl.ReportData);
+            // 
+            // stateMachineModule1
+            // 
+            this.stateMachineModule1.StateMachineStorageType = typeof(DevExpress.ExpressApp.StateMachine.Xpo.XpoStateMachine);
+            // 
+            // pivotChartModuleBase1
+            // 
+            this.pivotChartModuleBase1.ShowAdditionalNavigation = false;
+            // 
+            // NpoErmHrmSalaryWinApplication
+            // 
+            this.ApplicationName = "NpoMash.Erm.Hrm.Salary";
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.conditionalAppearanceModule1);
+            this.Modules.Add(this.validationModule1);
+            this.Modules.Add(this.iagErmModule1);
+            this.Modules.Add(this.iagXafExtModule1);
+            this.Modules.Add(this.cloneObjectModule1);
+            this.Modules.Add(this.viewVariantsModule1);
+            this.Modules.Add(this.pivotGridModule1);
+            this.Modules.Add(this.reportsModule1);
+            this.Modules.Add(this.chartModule1);
+            this.Modules.Add(this.stateMachineModule1);
+            this.Modules.Add(this.npoErmHrmModule1);
+            this.Modules.Add(this.npoErmHrmTestsModule1);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.chartWindowsFormsModule1);
+            this.Modules.Add(this.fileAttachmentsWindowsFormsModule1);
+            this.Modules.Add(this.pivotChartModuleBase1);
+            this.Modules.Add(this.pivotChartWindowsFormsModule1);
+            this.Modules.Add(this.pivotGridWindowsFormsModule1);
+            this.Modules.Add(this.reportsWindowsFormsModule1);
+            this.Modules.Add(this.scriptRecorderModuleBase1);
+            this.Modules.Add(this.scriptRecorderWindowsFormsModule1);
+            this.Modules.Add(this.treeListEditorsModuleBase1);
+            this.Modules.Add(this.treeListEditorsWindowsFormsModule1);
+            this.Modules.Add(this.validationWindowsFormsModule1);
+            this.Modules.Add(this.npoErmHrmWinModule1);
+            this.Modules.Add(this.iagXafExtWinModule1);
+            this.Modules.Add(this.module4);
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.SalaryWindowsFormsApplication_DatabaseVersionMismatch);
+            this.CustomizeLanguagesList += new System.EventHandler<DevExpress.ExpressApp.CustomizeLanguagesListEventArgs>(this.SalaryWindowsFormsApplication_CustomizeLanguagesList);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule module2;
+        private NpoMash.Erm.Hrm.Salary.NpoErmHrmSalaryModule module3;
+        private NpoMash.Erm.Hrm.Salary.Win.NpoErmHrmSalaryWinModule module4;
+        private DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule conditionalAppearanceModule1;
+        private DevExpress.ExpressApp.Validation.ValidationModule validationModule1;
+        private IntecoAG.ERM.IagErmModule iagErmModule1;
+        private IntecoAG.XafExt.IagXafExtModule iagXafExtModule1;
+        private DevExpress.ExpressApp.CloneObject.CloneObjectModule cloneObjectModule1;
+        private DevExpress.ExpressApp.ViewVariantsModule.ViewVariantsModule viewVariantsModule1;
+        private DevExpress.ExpressApp.PivotGrid.PivotGridModule pivotGridModule1;
+        private DevExpress.ExpressApp.Reports.ReportsModule reportsModule1;
+        private DevExpress.ExpressApp.Chart.ChartModule chartModule1;
+        private DevExpress.ExpressApp.StateMachine.StateMachineModule stateMachineModule1;
+        private NpoErmHrmModule npoErmHrmModule1;
+        private DevExpress.ExpressApp.Chart.Win.ChartWindowsFormsModule chartWindowsFormsModule1;
+        private DevExpress.ExpressApp.FileAttachments.Win.FileAttachmentsWindowsFormsModule fileAttachmentsWindowsFormsModule1;
+        private DevExpress.ExpressApp.PivotChart.PivotChartModuleBase pivotChartModuleBase1;
+        private DevExpress.ExpressApp.PivotChart.Win.PivotChartWindowsFormsModule pivotChartWindowsFormsModule1;
+        private DevExpress.ExpressApp.PivotGrid.Win.PivotGridWindowsFormsModule pivotGridWindowsFormsModule1;
+        private DevExpress.ExpressApp.Reports.Win.ReportsWindowsFormsModule reportsWindowsFormsModule1;
+        private DevExpress.ExpressApp.ScriptRecorder.ScriptRecorderModuleBase scriptRecorderModuleBase1;
+        private DevExpress.ExpressApp.ScriptRecorder.Win.ScriptRecorderWindowsFormsModule scriptRecorderWindowsFormsModule1;
+        private DevExpress.ExpressApp.TreeListEditors.TreeListEditorsModuleBase treeListEditorsModuleBase1;
+        private DevExpress.ExpressApp.TreeListEditors.Win.TreeListEditorsWindowsFormsModule treeListEditorsWindowsFormsModule1;
+        private DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule validationWindowsFormsModule1;
+        private Hrm.Win.NpoErmHrmWinModule npoErmHrmWinModule1;
+        private Tests.NpoErmHrmTestsModule npoErmHrmTestsModule1;
+        private IntecoAG.XafExt.Win.IagXafExtWinModule iagXafExtWinModule1;
+    }
+}
--- src/NpoMash.Erm/NpoMash.Erm.Hrm/NpoMash.Erm.Hrm.Tests/NpoMash.Erm.Hrm.Tests.AppWeb/Global.asax
+++
@@ -1,1 +0,0 @@
-<%@ Application Language="C#" CodeBehind="Global.asax.cs" Inherits="NpoMash.Erm.Hrm.Tests.Web.Global"%>
